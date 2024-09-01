3c2e387ca56f36b27b214b73d4138a0ab802e9bd
chrispaganon <info@chrispaganon.com>
2024-09-01 13:20:01 -0400

    add basic primevue

---
+++ app/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Home</h1>
+  <Button @click="(e) => console.log(e)">Click me</Button>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  devtools: { enabled: true },
+  primevue: {
+    autoImport: true,
+  }
 })
