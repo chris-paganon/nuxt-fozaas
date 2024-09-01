0132153bfc8e32a772d09c4c9240818889133381
chrispaganon <info@chrispaganon.com>
2024-09-01 13:39:08 -0400

    fix some formatting

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,4 @@
 <template>
   <h1>Home</h1>
-  <Button @click="(e) => console.log(e)">Click me</Button>
+  <Button @click="(e) => console.log(e)" class="ml-5"> Click me </Button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,15 +6,13 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-04-03',
-  modules: [
-    '@primevue/nuxt-module'
-  ],
+  modules: ['@primevue/nuxt-module'],
   devtools: { enabled: true },
   primevue: {
     options: {
       theme: {
         preset: Lara,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
